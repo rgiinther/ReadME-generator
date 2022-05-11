3cf6e3f801d773d77a6414c00dd702157fe30f28
rgiinther <r.giin95@gmail.com>
2022-05-10 20:22:20 -0500

    added licenses and read me file

--- Develop/generateMarkdown.js
+++ Develop/generateMarkdown.js
@@ -1,7 +1,19 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {
 
+function renderLicenseBadge(license) {
+ if (license === 'Apache 2.0'){
+   return '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'
+ };
+ if (license === 'MIT'){
+  return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'
+};
+if (license === 'GNU GPL V3'){
+  return '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)'
+};
+if (license === 'None'){
+  return ''
+};
 }
 
 // TODO: Create a function that returns the license link
@@ -19,7 +31,30 @@ function renderLicenseSection(license) {
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+${renderLicenseBadge(data.license)}
 
+# Description
+${data.description}
+
+# Table of Contents
+
+# Installation
+${data.installation}
+
+# Usage
+${data.usage}
+
+#License
+${data.license}
+
+#Contributors
+${data.contributors}
+
+# Tests
+${data.tests}
+
+# Questions
+${data.questions}
 `;
 }
 
--- index.js
+++ index.js
@@ -2,78 +2,80 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const generatePage = require('./Develop/generateMarkdown.js');
+
 // TODO: Create an array of questions for user input
+const questions = [
+  {
+    type: 'input',
+    name: 'title',
+    message: 'What is your project name?'
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Enter your project description'
+  },
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'Please provide intallation instructions'
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'Please provide information for usage of this project'
+  },
+  {
+    type: 'list',
+    name: 'license',
+    message: 'What Licenses does this project have?',
+    choices: ['Apache 2.0', 'MIT', 'GNU GPL V3', 'None']
+  },
+  {
+    type: 'input',
+    name: 'contributors',
+    message: 'Please list contributors'
+  }, 
+  {
+      type: 'input',
+      name: 'tests',
+      message: 'Please provide project tests:'
+  },
+  {
+    type: 'input',
+    name: 'github',
+    message: 'what is your github username?'
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'what is your email?'
+  },
+];
 
-inquirer
-  .prompt([
-    {
-        type: 'input',
-        name: 'Title',
-        message: 'What is your project name?'
-      },
-      {
-        type: 'input',
-        name: 'description',
-        message: 'Enter your project description'
-      },
-      {
-        type: 'input',
-        name: 'Table of Contents',
-        message: 'Provide some information for the Table of Contents'
-      },
-      {
-        type: 'input',
-        name: 'Installation',
-        message: 'Please provide intallation instructions'
-      },
-      {
-        type: 'input',
-        name: 'Usage',
-        message: 'Please provide information for usage of this project'
-      },
-      {
-        type: 'input',
-        name: 'License',
-        message: 'What Licenses does this project have?'
-      },
-      {
-        type: 'input',
-        name: 'contributors',
-        message: 'Please list contributors'
-      }, 
-      {
-          type: 'input',
-          name: 'Tests',
-          message: 'Please provide project tests:'
-      },
-      {
-          type: 'input',
-          name: 'Questions',
-          message: 'Type any questions you have'
-      }
-    ])
-inquirer.promptUser(questions)
-    .then(answers => console.log(answers))
-    .then(promptProject)
-    .then(projectAnswers => console.log(projectAnswers))
-
-    fs.writeFile(filename, JSON.stringify(data, null, '\t'), err =>
-      err ? console.log(err) : console.log('Readme successfully written!')
-    );
- 
 
 // TODO: Create a function to write README file
-//function writeToFile(fileName, data) {}
-//fs.writeFile("README.md", generate(fileName, data), function(err){
-    //if (err) {
-        //throw err;
-   // };
+function writeToFile(template) {
+fs.writeFile('README.md', template, function(err){
+    if (err) {
+        throw err;
+   };
 
-   // console.log("New README file created with success!");
- // });
+   console.log("New README file created with success!");
+ });
+}
+
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer.prompt (questions)
+  .then(answers => {
+    console.log(answers)
+    var readMeTemplate = generatePage (answers);
+    console.log(readMeTemplate);
+    writeToFile(readMeTemplate);
+  })
+}
 
 // Function call to initialize app
 init();
